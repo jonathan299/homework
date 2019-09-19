8739284bbb41cb5d3777a042b11dd0be9072f44d
jonathan299 <55444026+jonathan299@users.noreply.github.com>
2019-09-19 08:39:54 -0400

    Add files via upload

---
+++ 2019-09-19-Homework/2019-09-19-Homework/Main.cpp
@@ -0,0 +1,13 @@
+//name: jonathan vasquez 
+//intro to computer science
+// Mr. Willams 
+// 2019-09-19-homework
+
+#include <iostream>
+
+int main()
+{
+	std::cout	<< "name: jonathan vasquez" << std::endl
+				<< "class schedule" << std::endl 
+				
+}
